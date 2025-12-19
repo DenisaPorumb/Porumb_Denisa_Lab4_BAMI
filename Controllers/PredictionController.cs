96a5a204ee68082e7eec1afff5dee7b35bd04e9d
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2025-12-19 13:41:41 +0200

    sarcina laborator time prediction

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -1,24 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.ML;
-using static Porumb_Denisa_Lab4.PricePredictionModel;
+using PriceML = Porumb_Denisa_Lab4.PricePredictionModel;
+using TimeML = Porumb_Denisa_Lab4.TimePrediction;
 
 namespace Porumb_Denisa_Lab4.Controllers
 {
     public class PredictionController : Controller
     {
-        public IActionResult Price(ModelInput input)
+        public IActionResult Price(PriceML.ModelInput input)
         {
             // Load the model
             MLContext mlContext = new MLContext();
             // Create predection engine related to the loaded train model
             ITransformer mlModel =
            mlContext.Model.Load("PricePredictionModel.mlnet", out var modelInputSchema);
-            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput,
-           ModelOutput>(mlModel);
+            var predEngine = mlContext.Model.CreatePredictionEngine<PriceML.ModelInput,
+           PriceML.ModelOutput>(mlModel);
 
             // Try model on sample data to predict fair price
-            ModelOutput result = predEngine.Predict(input);
+            PriceML.ModelOutput result = predEngine.Predict(input);
             ViewBag.Price = result.Score;
+            return View(input);
+        }
+
+        public IActionResult Time(TimeML.ModelInput input)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            // Create predection engine related to the loaded train model
+            ITransformer mlModel =
+           mlContext.Model.Load("TimePrediction.mlnet", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<TimeML.ModelInput,
+           TimeML.ModelOutput>(mlModel);
+
+            // Try model on sample data to predict fair price
+            TimeML.ModelOutput result = predEngine.Predict(input);
+            ViewBag.Time = result.Score;
             return View(input);
         }
 
