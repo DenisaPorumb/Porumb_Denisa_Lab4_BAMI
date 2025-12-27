b044794e588876230fc01618b481c7778253bee0
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2025-12-27 14:19:59 +0200

    exercitiul 2 gata

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.ML;
+using Porumb_Denisa_Lab4.Models;
 using PriceML = Porumb_Denisa_Lab4.PricePredictionModel;
 using TimeML = Porumb_Denisa_Lab4.TimePrediction;
+using Porumb_Denisa_Lab4.Data;
 
 namespace Porumb_Denisa_Lab4.Controllers
 {
     public class PredictionController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public PredictionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Price() => View();
 
         [HttpPost]
-        public IActionResult Price(PriceML.ModelInput input)
+        public async Task<IActionResult> Price(PriceML.ModelInput input)
         {
             // Load the model
             MLContext mlContext = new MLContext();
@@ -24,6 +34,21 @@ namespace Porumb_Denisa_Lab4.Controllers
             // Try model on sample data to predict fair price
             PriceML.ModelOutput result = predEngine.Predict(input);
             ViewBag.Price = result.Score;
+            
+
+            var history = new PredictionHistory
+            {
+                PassengerCount = input.Passenger_count,
+                TripTimeInSecs = input.Trip_time_in_secs,
+                TripDistance = input.Trip_distance,
+                PaymentType = input.Payment_type,
+                PredictedPrice = result.Score,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.PredictionHistories.Add(history);
+            await _context.SaveChangesAsync();
+
             return View(input);
         }
 
@@ -48,6 +73,15 @@ namespace Porumb_Denisa_Lab4.Controllers
             TimeML.ModelOutput result = predEngine.Predict(input);
             ViewBag.Time = result.Score;
             return View(input);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> History()
+        {
+            var history = await _context.PredictionHistories
+            .OrderByDescending(p => p.CreatedAt)
+            .ToListAsync();
+            return View(history);
         }
 
     }
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Porumb_Denisa_Lab4.Models;
+
+namespace Porumb_Denisa_Lab4.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<PredictionHistory> PredictionHistories { get; set; }
+    }
+}
