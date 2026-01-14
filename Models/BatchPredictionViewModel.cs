59396d8555509be5b2e5e95de204851e3b033ee1
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2026-01-14 20:06:14 +0200

    lab4_partea5

---
+++ Models/BatchPredictionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PriceML = Porumb_Denisa_Lab4.PricePredictionModel;
+namespace Porumb_Denisa_Lab4.Models
+{
+    public class BatchPredictionViewModel
+    {
+        public IFormFile? File { get; set; }     
+        
+        // Rezultatele (predicțiile) pentru fiecare rând din fișier
+        public List<PriceML.ModelOutput>? Predictions { get; set; }
+        public string? ErrorMessage { get; set; } 
+    }
+}
